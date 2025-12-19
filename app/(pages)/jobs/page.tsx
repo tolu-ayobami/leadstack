491fa3b2652ccc2ce8d8021923a383ebca6298d7
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-12-19 18:23:59 +0100

    update

--- app/(pages)/jobs/page.tsx
+++ app/(pages)/jobs/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import { MotionItem } from '@/components/animations/MotionItems'
 import { partners } from '@/utils/Usedata'
 import { fadeDownVariants } from '@/utils/Variants'
+import { management1, management2 } from '@/utils/Managementdata'
+import { MapPin, Search, Calendar, List, Grid } from "lucide-react";
+import { CurrentOpening } from '@/components/sections/CurrentOpening'
 
 
 const Page = () => {
@@ -90,7 +93,7 @@ const Page = () => {
         </div>
 
         <div>
-          <h1 className="font-noto text-2xl font-semibold max-w-xl">
+          <h1 className="font-noto lg:text-2xl font-semibold max-w-xl">
             We’ve been using Leadstack to kick start every new lorem and can’t imagine working without it.
           </h1>
         </div>
@@ -102,11 +105,110 @@ const Page = () => {
         </div>
       </div>
 
-      <div className=''>
+      <div className='bg-[#F9FAFB] flex flex-col justify-center items-center text-center py-10  gap-6'>
 
-        <h1 className="font-noto text-2xl font-semibold max-w-xl">Efficient management of staff through our easy to use features</h1>
+        <h1 className="font-noto lg:text-4xl font-semibold max-w-xl text-[#0A2E65]">Efficient management of staff through our easy to use features</h1>
+
+
+        <div className=" w-full overflow-x-auto flex  justify-center py-8 gap-10">
+          {management1.map((logo, idx) => (
+            <div key={idx} className="flex items-center gap-2 flex-shrink-0">
+              <div className='hidden lg:flex flex flex-col gap-1'>
+                <span className="text-sm font-medium">{logo.name1}</span>
+                <span className="text-sm font-medium">{logo.name2}</span>
+              </div>
+              <div className='p-3' style={{ backgroundColor: logo.color }}>
+                <Image
+                  src={logo.logo}
+                  alt={logo.name1}
+                  width={56}
+                  height={56}
+                  className="h-10 w-auto object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+        <div className="w-full overflow-x-auto flex justify-center py-8 gap-12">
+          {management2.map((logo, idx) => (
+            <div key={idx} className="flex items-center gap-3 flex-shrink-0"
+            >
+              <div className='hidden lg:flex flex flex-col gap-1 text-left'>
+                <span className="text-sm font-medium">{logo.name1}</span>
+                <span className="text-sm font-medium">{logo.name2}</span>
+              </div>
+              <div className='p-3' style={{ backgroundColor: logo.color }}>
+                <Image
+                  src={logo.logo}
+                  alt={logo.name1}
+                  width={56}
+                  height={56}
+                  className="h-10 w-auto object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-6">
+          <h1 className="font-noto lg:text-4xl font-semibold max-w-xl text-[#0A2E65]">Looking for the next great chapter in your career, Check here</h1>
+        </div>
+
+        <div className='flex items-center mt-6 gap-2 flex-wrap max-sm:px-4'>
+
+          <div className='flex items-center gap-2 border rounded-md w-[250px] p-3'>
+            <Search className="w-4 h-4 text-[#535768]" />
+            <input type='text' placeholder='Search Job, keywords, companies, skills...' className='text-[14px] borer-none outline-none' />
+          </div>
+
+          <div className=' flex items-center  justify-center gap-2 border rounded-md w-[200px] p-3'>
+            <MapPin className="w-4 h-4 text-[#535768]" />
+            <button type='button' className='text-[14px] text-[#535768]'>
+              Select Locations
+            </button>
+          </div>
+
+          <div className=' flex items-center gap-2 border justify-center rounded-md w-[200px] p-3'>
+            <Calendar className="w-4 h-4 text-[#535768}" />
+            <button type='button' className='text-[14px] text-[#535768]'>
+              Date Range
+            </button>
+          </div>
+
+
+          <button type='button' className='bg-[#1D8EE6] text-white p-3 rounded-lg'>
+            Search
+          </button>
+
+        </div>
 
       </div>
+
+      <CurrentOpening />
+
+
+      <div className="bg-gray-100  w-full  py-8 flex flex-col gap-2 justify-center items-center">
+          <Image src="/images/Avatar group.png" alt="avatar" width={80} height={80} />
+          <p>Still have questions?</p>
+          <p className="text-[#667085] font-inter text-sm flex-wrap max-w-sm text-cecnter">Can’t find the answer you’re looking for? Please chat to our friendly team.</p>
+
+          <button type='button' className='bg-[#1D8EE6]  px-4 text-white py-2 rounded-lg'>
+           Get in touch
+          </button>
+        </div>
+
+
+        <div className=' mt-10 flex flex-col justify-center gap-2 items-center'>
+
+          <div className='flex items-center gap-1'>
+            <p className='text-[#98A2B3] text-[14px]'>Powered by </p>
+             <Image src="/images/jobslogo.png" alt="jobslogo" width={100} height={100} />
+          </div>
+
+          <p className='text-[#98A2B3] text-[14px]'>© 2077 Untitled UI. All rights reserved.</p>
+        </div>
 
 
 
---
+++ utils/Managementdata.tsx
@@ -0,0 +1,65 @@
+import { color } from "framer-motion";
+
+export const management1 = [
+    {
+        id: 1,
+        name1: "Employe",
+        name2:"Management",
+        logo: "/icons/Employees.svg",
+        color:"#E8F4FC"
+    },
+    {
+        id: 2,
+        name1: "Financial",
+        name2:"Management",
+        logo: "/icons/Finance.svg",
+        color:"#FFF7EC"
+    },
+    {
+        id: 3,
+        name1: "Time",
+        name2:"Management",
+        logo: "/icons/Time Management.svg",
+        color:"#EBFBED"
+    },
+    {
+        id: 4,
+        name1: "Report & ",
+        name2:"Analytics",
+        logo: "/icons/reports.svg",
+        color:"#EBE5FC"
+    },
+
+];
+
+export const management2 = [
+    {
+        id: 1,
+        name1: "Recruitment &",
+         name2:"Hiring",
+        logo: "/icons/Case.svg",
+        color:"#F7EEFC"
+    },
+    {
+        id: 2,
+        name1: "Loan",
+        name2:"Management",
+        logo: "/icons/Shapes.svg",
+        color:"#F1FBFF"
+    },
+    {
+        id: 3,
+        name1: "Document ",
+        name2:"Management",
+        logo: "/icons/Documents.svg",
+        color:"#FFEBEA"
+    },
+    {
+        id: 4,
+        name1: "Report &",
+        name2:"Analyticst",
+        logo: "/icons/Path 3.svg",
+        color:"#EFFBFF"
+    },
+
+];
